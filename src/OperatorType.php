996902c60e4a6d28c73ffa83d92c907ec3fe079c
Aaron Adrian <aaroncadrian@gmail.com>
2018-06-20 18:04:32 -0400

    Add OperatorType enum

--- src/OperatorType.php
+++ src/OperatorType.php
@@ -2,7 +2,18 @@
 
 namespace AaronAdrian\StringOperatorComparison;
 
-class OperatorType
-{
+use MyCLabs\Enum\Enum;
 
+class OperatorType extends Enum
+{
+    const LESS_THAN = '<';
+    const LESS_THAN_OR_EQUAL_TO = '<=';
+    const GREATER_THAN = '>';
+    const GREATER_THAN_OR_EQUAL_TO = '>=';
+    const EQUALS = '=';
+    const EQUAL_TO = '==';
+    const IDENTICAL_TO = '===';
+    const DOES_NOT_EQUAL = '<>';
+    const NOT_EQUAL_TO = '!=';
+    const NOT_IDENTICAL_TO = '!==';
 }
